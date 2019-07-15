8869816a4bf200f1556bf850e547bc47ec3dd639
Simon Egli <git@egli.online>
2019-07-15 21:36:38 +0200

    Adding compilable bluetooth scanner
    But it does not work on my machine

---
+++ src/bluetooth.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class Bluetooth {
+  public:
+    int scan(void);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "sqlite.h"
+#include "bluetooth.h"
 int main(void)
 {
   Sqlite sqlite;
   sqlite.open();
   sqlite.close();
+  Bluetooth bluetooth;
+  bluetooth.scan();
   return 0;
 }
