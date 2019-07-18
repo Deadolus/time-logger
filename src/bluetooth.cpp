134fae84a255c21f8e5d70d05e11303cfd71d837
Simon Egli <git@egli.online>
2019-07-18 13:27:44 +0200

    Cppifying bluetooth.cpp a bit

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -9,42 +9,42 @@
 #include <bluetooth/hci_lib.h>
 #include <vector>
 #include <string>
+#include <array>
 
 std::vector<std::pair<std::string, std::string>> Bluetooth::scan(void)
 {
-    inquiry_info *ii = NULL;
-    int max_rsp, num_rsp;
-    int dev_id, sock, len, flags;
-    int i;
-    char addr[19] = { 0 };
-    char name[248] = { 0 };
+  constexpr int max_rsp = 255;
+  constexpr int len = 8;
+  constexpr int flags = IREQ_CACHE_FLUSH;
+  int num_rsp;
+  int dev_id;
+  int sock;
+  int i;
+  std::array<inquiry_info, max_rsp> ii2{};
+  inquiry_info *ii = ii2.data(); //ugly
+  std::vector<std::pair<std::string, std::string>> devices;
 
-    dev_id = hci_get_route(NULL);
-    sock = hci_open_dev( dev_id );
-    if (dev_id < 0 || sock < 0) {
-        perror("opening socket");
-        return devices;
-    }
-
-    len  = 8;
-    max_rsp = 255;
-    flags = IREQ_CACHE_FLUSH;
-    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
-
-    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
-    if( num_rsp < 0 ) perror("hci_inquiry");
-
-    std::vector<std::pair<std::string, std::string>> devices;
-    for (i = 0; i < num_rsp; i++) {
-        ba2str(&(ii+i)->bdaddr, addr);
-        memset(name, 0, sizeof(name));
-        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name),
-            name, 0) < 0)
-        strcpy(name, "[unknown]");
-        devices.push_back(std::make_pair<std::string, std::string>(addr, name));
-    }
-
-    free( ii );
-    close( sock );
+  dev_id = hci_get_route(nullptr);
+  sock = hci_open_dev( dev_id );
+  if (dev_id < 0 || sock < 0) {
+    perror("opening socket");
     return devices;
+  }
+
+
+  num_rsp = hci_inquiry(dev_id, len, max_rsp, nullptr, &ii, flags);
+  if( num_rsp < 0 ) perror("hci_inquiry");
+
+  for (i = 0; i < num_rsp; i++) {
+    std::array<char, 19> addr{};
+    std::array<char, max_rsp> name{};
+    ba2str(&(ii+i)->bdaddr, addr.data());
+    if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name),
+          name.data(), 0) < 0)
+      strcpy(name.data(), "[unknown]");
+    devices.push_back(std::make_pair<std::string, std::string>(addr.data(), name.data()));
+  }
+
+  close( sock );
+  return devices;
 }
